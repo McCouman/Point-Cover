8ccf8977559333fe6b73a48dfd0f233bbf85198d
Michael McCouman jr. <support@wikibyte.org>
2013-10-25 16:50:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php
+//injects to wikibyte images
 if (!isset($_GET['name']) || empty($_GET['name'])) die();
-$name = escapeshellarg($_GET['name']);
-$datei = explode("\n",`ls . | grep $name`);
-$datei = $datei[0];
-if (empty($datei)) die();
-$finfo = new finfo(FILEINFO_MIME);
-$mime = $finfo->file($datei);
+
+  $name = escapeshellarg($_GET['name']);
+    $datei = explode("\n",`ls . | grep $name`);
+    $datei = $datei[0];
+
+  if (empty($datei)) die();
+    $finfo = new finfo(FILEINFO_MIME);
+    $mime = $finfo->file($datei);
+
 header("Content-Type: ".$mime);
+
 readfile($datei);
